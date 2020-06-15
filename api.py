89e9a6ab532f781025fb13dc927ffda0f5190160
Calum Ludwig <calum.ludwig@gmail.com>
2020-06-15 21:34:50 +1000

    Initial commit of api.py.

--- api.py
+++ api.py
@@ -1,5 +1,8 @@
+import time
+
 from flask import Flask, request, make_response
-import os, json, time
+
+from application import Configuration
 
 app = Flask(__name__)
 json_data = []
@@ -31,7 +34,7 @@ def time_elapsed():
 
 def get_response_data(request_uri, request_method):
     if not json_data:
-        open_config_files()
+        Configuration.open_config_files(json_data)
 
     time_elapsed()
 
@@ -41,38 +44,17 @@ def get_response_data(request_uri, request_method):
             response_file = item['response_file']
 
             if operation == request_method and response_file:
-                return {'response': open_response_file(response_file), 'status_code': item['status_code']}
+                return {'response': Configuration.open_response_file(response_file), 'status_code': item['status_code']}
 
     return {'response': 'NA', 'status_code': 404}
 
 
 def define_api_rules():
     if not json_data:
-        open_config_files()
+        Configuration.open_config_files(json_data)
 
     for item in json_data:
         app.add_url_rule(str(item['uri']), endpoint='index', view_func=index, provide_automatic_options=False)
-
-
-def open_config_files():
-    path_to_json_files = 'config/'
-    json_files = [pos_json for pos_json in os.listdir(path_to_json_files) if pos_json.endswith('json')]
-
-    for dex, js in enumerate(json_files):
-        with open(os.path.join(path_to_json_files, js)) as json_file:
-            json_array = json.load(json_file)
-            for item in json_array:
-                json_details = {'uri': item['uri'], 'status_code': item['status_code'],
-                                'response_file': item['response_file'], 'operation': item['operation']}
-                json_data.append(json_details)
-
-
-def open_response_file(filename):
-    path_to_resp_files = 'response_files/'
-
-    with open(os.path.join(path_to_resp_files, filename)) as json_file:
-        json_resp_data = json.load(json_file)
-        return json_resp_data
 
 
 if __name__ == '__main__':
